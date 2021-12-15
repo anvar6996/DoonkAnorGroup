aff780416f2c1ee66441a3931cf07394ce56a2f2
anvar6996 <juraqulovanvar69@gmail.com>
2021-12-15 14:29:24 +0500

    Anvar

--- app/src/main/java/uz/anorgroup/doonkanorgroup/presenter/screens/pages/ChatPage.kt
+++ app/src/main/java/uz/anorgroup/doonkanorgroup/presenter/screens/pages/ChatPage.kt
@@ -2,8 +2,8 @@ package uz.anorgroup.doonkanorgroup.presenter.screens.pages
 
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
-import uz.anorgroup.doonk.R
-import uz.anorgroup.doonk.databinding.ChatPageBinding
+import uz.anorgroup.doonkanorgroup.R
+import uz.anorgroup.doonkanorgroup.databinding.ChatPageBinding
 
 class ChatPage : Fragment(R.layout.chat_page) {
     private val bind by viewBinding(ChatPageBinding::bind)
