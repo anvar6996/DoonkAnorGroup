f08099434aeb3c19be2a3a379b95484be7e14772
anvar6996 <juraqulovanvar69@gmail.com>
2021-12-16 15:35:49 +0500

    Anvar

--- app/src/main/java/uz/anorgroup/doonkanorgroup/data/api/AuthApi.kt
+++ app/src/main/java/uz/anorgroup/doonkanorgroup/data/api/AuthApi.kt
@@ -1,14 +1,18 @@
-package uz.anorgroup.doonkanorgroup.api
+package uz.anorgroup.doonkanorgroup.data.api
 
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
+import uz.anorgroup.doonkanorgroup.data.request.ContinueSignUpRequest
 import uz.anorgroup.doonkanorgroup.data.request.LoginRequest
 import uz.anorgroup.doonkanorgroup.data.request.RegisterRequest
+import uz.anorgroup.doonkanorgroup.data.request.VerifyRequest
 import uz.anorgroup.doonkanorgroup.data.responce.LoginResponse
 import uz.anorgroup.doonkanorgroup.data.responce.RegisterResponse
+import uz.anorgroup.doonkanorgroup.data.responce.VerifyResponse
 
 interface AuthApi {
+
     @POST("signIn")
     suspend fun login(@Body data: LoginRequest): Response<LoginResponse>
 
@@ -16,8 +20,8 @@ interface AuthApi {
     suspend fun register(@Body data: RegisterRequest): Response<RegisterResponse>
 
     @POST("signUp")
-    suspend fun continueSingUp(@Body data: RegisterRequest): Response<RegisterResponse>
+    suspend fun continueSingUp(@Body data: ContinueSignUpRequest): Response<RegisterResponse>
 
-//    @POST("verify")
-//    suspend fun veryfyCode(@Body data: SmsVeryfyRequest): Response<VeriyfyResponce>
+    @POST("verify")
+    suspend fun veryfyCode(@Body data: VerifyRequest): Response<VerifyResponse>
 }
