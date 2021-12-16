897e45d5fa0411c4814b6d1aa56a633c0ce52100
anvar6996 <juraqulovanvar69@gmail.com>
2021-12-16 16:59:44 +0500

    Anvar

--- app/src/main/java/uz/anorgroup/doonkanorgroup/domain/repository/repositoryimpl/AuthRepositoryImpl.kt
+++ app/src/main/java/uz/anorgroup/doonkanorgroup/domain/repository/repositoryimpl/AuthRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package uz.anorgroup.doonkanorgroup.domain.repository.repositoryimpl
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import uz.anorgroup.doonkanorgroup.data.api.AuthApi
+import uz.anorgroup.doonkanorgroup.data.pref.MyPref
 import uz.anorgroup.doonkanorgroup.data.request.LoginRequest
 import uz.anorgroup.doonkanorgroup.data.request.RegisterRequest
 import uz.anorgroup.doonkanorgroup.data.request.VerifyRequest
@@ -10,9 +13,9 @@ import uz.anorgroup.doonkanorgroup.data.responce.VerifyResponse
 import uz.anorgroup.doonkanorgroup.domain.repository.AuthRepository
 import javax.inject.Inject
 
-class AuthRepositoryImpl @Inject constructor() : AuthRepository {
-    override fun login(request: LoginRequest): Flow<Result<LoginResponse>> {
-        TODO("Not yet implemented")
+class AuthRepositoryImpl @Inject constructor(private val api: AuthApi, private val pre: MyPref) : AuthRepository {
+
+    override fun login(request: LoginRequest): Flow<Result<LoginResponse>> = flow {
     }
 
     override fun register(request: RegisterRequest): Flow<Result<RegisterResponse>> {
